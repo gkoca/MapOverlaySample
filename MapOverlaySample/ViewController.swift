174efa0c6a44e06fab29e0b9e9e39affaf6c5a1c
A. Gökhan KOCA <gokhan.koca@outlook.com.tr>
2018-02-20 20:41:06 +0300

    Project created

---
+++ MapOverlaySample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MapOverlaySample
+//
+//  Created by Gökhan KOCA on 20.02.2018.
+//  Copyright © 2018 gkoca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
